b275d0ff1e099e9958bc03bfab8e80cb76cef091
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-17 21:23:29 +0530

    Add files via upload

---
+++ 19_conditional.py
@@ -0,0 +1,15 @@
+a=int(input("enter your age:"))
+
+if(a>=18):
+    print("you are above the age of concent")
+    print("good for you")
+
+elif(a<0):
+    print("you enter invalid age")
+
+elif(a==0):
+    print("you are entering 0. Which is invalid")
+
+else:
+    print("you are under age")
+
