5b877cc0485d76c63d18f2916b8bfbd0227ed6a2
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-19 23:31:00 +0530

    Add examples of break and continue in loops

---
+++ 24_break_and_continue.py
@@ -0,0 +1,10 @@
+for i in range(100):
+    if i ==33:
+        break      #exit the loop when i is 33
+    print(i)
+
+
+for i in range(100):
+    if i ==33:
+        continue      #skip the iteration when i is 33
+    print(i)
