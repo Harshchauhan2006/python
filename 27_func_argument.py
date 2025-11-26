d21e59d7fa0a5e65e00a3bfdcc3c05c90d85d9bc
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-26 23:31:41 +0530

    Add goodDay function to greet users

---
+++ 27_func_argument.py
@@ -0,0 +1,8 @@
+def goodDay(name, ending):
+    
+    print("good day " + name)
+    print(ending)
+          
+goodDay("Harsh" , "Thank you") 
+goodDay("Rohan" , "Thank you")
+goodDay("divya" , "Thank you")
