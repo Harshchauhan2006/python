d8679d4acf45b9148507e939a3a30bf2a4f4cb0a
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-26 23:27:32 +0530

    Add function to calculate average of three numbers

--- 25_functions.py
+++ 25_functions.py
@@ -1,1 +1,12 @@
+def avg():
+            
+    a=int(input("Enter your number: "))
+    b=int(input("Enter your number: "))
+    c=int(input("Enter your number: "))
 
+    average=(a+b+c)/3
+    print(average)
+
+avg()
+print("thank you")
+avg()
