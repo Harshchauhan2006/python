178df4de1fc62fa04a70069e9e6abe4a956d7518
Harsh Chauhan <130071682+Harshchauhan2006@users.noreply.github.com>
2025-11-27 18:59:23 +0530

    Add factorial function with user input

--- 29_recursion.py
+++ 29_recursion.py
@@ -1,1 +1,6 @@
-
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1    
+    return n * factorial(n - 1)
+n=int(input("Enter a number: "))
+print(f"the factorial of this number is: {factorial(n)}")
